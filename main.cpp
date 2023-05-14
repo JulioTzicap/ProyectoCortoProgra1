720ccf23efee23c04f26112ea59f036d66ffc600
Shairy Gomez <132172616+ShairyGC@users.noreply.github.com>
2023-05-14 16:08:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ void fichas (int filas){ //Declaramo una función que dibuje las fichas.
         cout<<"|";//Figuras del juego
     }
     cout<<endl;
-
 }
 void graficos (int fila_a, int fila_b, int fila_c) {//declarmos una funcion que llamara a la función anterior para dibujar las filas de el juego.
     cout<<"Fila 1:          "; fichas (fila_a);
@@ -32,7 +31,6 @@ void limpiar (int pantalla){//Función que nos ayuda a limpiar la pantalla cada 
         system("clear"); // Linux y macOS.
     #endif
 }
-
 int main (){
     string jugador_1; string jugador_2; //Variables de los nombres
     int puntos = 0;
@@ -40,20 +38,17 @@ int main (){
     int limpio = 0;//Variables para limpiar
     int quitar_fila, quitar_ficha;//Variables para seleccionar la fila y eliminar las fichas.
     char tecla;//Variable es para volver a jugar.
-
     nombres (jugador_1, jugador_2);
     cout<<"Ingrese el nombre del jugador 1: ";
     cin>>jugador_1;
     cout<<"Ingrese el nombre del jugador 2: ";
     cin>>jugador_2;
-
     do {
         limpiar (limpio);
         puntos = puntos;
         puntos_2 = puntos_2;
         int fila_1=3, fila_2=5, fila_3 = 7; //Variables para los gráficos.
         bool turno_jugador_1 = true;//Variable para verificar que turno corresponde, aqui se usa la variable booleana requerida.
-
         while (fila_1 + fila_2 + fila_3 > 1){
             if (turno_jugador_1) {
                 cout<<"Es turno de: "<<jugador_1<<endl;
@@ -65,53 +60,43 @@ int main (){
             graficos(fila_1, fila_2, fila_3);
             cout<<"Que fila desea modificar (del 1 al 3): ";
             cin>>quitar_fila;
-
             //Validación de fila
             while (quitar_fila < 1 || quitar_fila > 3 || (quitar_fila == 1 && fila_1 == 0) || (quitar_fila == 2 && fila_2 == 0) || (quitar_fila == 3 && fila_3 == 0)) {
                 cout << "Por favor, ingrese un número válido de fila: ";
                 cin >> quitar_fila;
             }
-
             if (quitar_fila == 1) {
                 cout<<"Que ficha desea quitar (1 al "<<fila_1<<") :";
                 cin>>quitar_ficha;
-
                 //Validación de ficha.
                 while (quitar_ficha < 1 || quitar_ficha > fila_1) {
                     cout << "Por favor, ingrese un número válido de fichas a quitar (1 al " << fila_1 << "): ";
                     cin >> quitar_ficha;
                 }
-
                 fila_1 -= quitar_ficha;
             } else if (quitar_fila == 2) {
                 cout<<"Que ficha desea quitar (1 al "<<fila_2<<"): ";
                 cin>>quitar_ficha;
-
                 //Validación de ficha
                 while (quitar_ficha < 1 || quitar_ficha > fila_2) {
                     cout << "Por favor, ingrese un número válido de fichas a quitar (1 al " << fila_2 << "): ";
                     cin >> quitar_ficha;
                 }
-
                 fila_2 -= quitar_ficha;
             } else {
                 cout<<"Que ficha desea quitar (1 al "<<fila_3<<") :";
                 cin>>quitar_ficha;
-
                 //Validación de ficha
                 while (quitar_ficha < 1 || quitar_ficha > fila_3) {
                     cout << "Por favor, ingrese un número válido de fichas a quitar (1 al " << fila_3 << "): ";
                     cin >> quitar_ficha;
                 }
-
                 fila_3 -= quitar_ficha;
             }
-
             if((fila_1 + fila_2 + fila_3) > 1){
                 turno_jugador_1 = !turno_jugador_1;//Para cambiar de jugador
             }
             cout<<endl;
-
         }if((fila_1 + fila_2 + fila_3) == 1){
       //Sumatoria cuando gana el jugador 1
             if(turno_jugador_1) {
@@ -141,17 +126,14 @@ int main (){
             puntos_2+=1;
             cout<<"Puntos de: "<<jugador_1<<" son: "<<puntos<<endl;
             cout<<"Puntos de: "<<jugador_2<<" son: "<<puntos_2<<endl;
-
         }
         cout<<"\nPresione la letra C para continuar jugando.\nO Presione cualquier tecla para salir del juego \nSeguido de su seleccion presione enter\n";
         cin>>tecla;
-
     }while (tecla=='C' || tecla=='c');
     //Se determina quien es el ganador de la partida
     if (puntos>puntos_2){
         cout<<"\nEl ganador de la partida es "<<jugador_1<<" con un total de "<<puntos<<" puntos."<<endl;
         cout<<"El perdedor de la partida es "<<jugador_2<<" con un total de "<<puntos_2<<" puntos."<<endl;
-
     }else if (puntos_2>puntos){
          cout<<"\nEl ganador de la partida es "<<jugador_2<<" con un total de "<<puntos_2<<" puntos."<<endl;
          cout<<"El perdedor de la partida es "<<jugador_1<<" con un total de "<<puntos<<" puntos."<<endl;
@@ -160,6 +142,5 @@ int main (){
         cout<<"El jugador "<<jugador_1<<" con "<<puntos<<" puntos."<<endl;
         cout<<"El jugador "<<jugador_2<<" con "<<puntos_2<<" puntos."<<endl;
     }
-
     return 0;
 }
